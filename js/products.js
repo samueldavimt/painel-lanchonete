ccea958f6968411e9da517a3ee1f2e2f5feb2d06
Samuel Davi <samuel.davimont@gmail.com>
2022-02-28 19:30:26 -0300

    json produtos

---
+++ js/products.js
@@ -0,0 +1,98 @@
+
+const products = [
+
+    {
+        id:1,
+        nameSection: 'hamburguer',
+        img:'/assets/images/burger/burger2.jpg',
+        items: [
+            {
+                id:1, 
+                name:'Burger Milaneza',
+                img:'/assets/images/burger/burger1.jpg',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:10.00},
+                    {name:'Médio', size:'320g',price:15.00},
+                    {name:'Grande', size:'820g',price:22.00}
+
+                ]
+    
+            },
+
+            {
+                id:1, 
+                name:'Burger Milaneza',
+                img:'/assets/images/burger/burger1.jpg',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:10.00},
+                    {name:'Médio', size:'320g',price:15.00},
+                    {name:'Grande', size:'820g',price:22.00}
+
+                ]
+    
+            },
+
+            {
+                id:1, 
+                name:'Burger Milaneza',
+                img:'/assets/images/burger/burger1.jpg',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:10.00},
+                    {name:'Médio', size:'320g',price:15.00},
+                    {name:'Grande', size:'820g',price:22.00}
+
+                ]
+    
+            }
+
+        ]
+    },
+
+    {
+        id:2,
+        nameSection: 'Pizza',
+        img:'/assets/images/pizza/slider-pizza.jpg',
+        items: [
+            {
+                id:1, 
+                name:'Calabreza',
+                img:'/assets/images/pizza/pizza.png',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:15.00},
+                    {name:'Médio', size:'320g',price:17.00},
+                    {name:'Grande', size:'820g',price:26.00}
+
+                ]
+    
+            },
+
+            {
+                id:2, 
+                name:'4 Queijos',
+                img:'/assets/images/pizza/pizza2.png',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:20.00},
+                    {name:'Médio', size:'320g',price:24.00},
+                    {name:'Grande', size:'820g',price:28.00}
+
+                ]
+    
+            },
+
+            {
+                id:3, 
+                name:'Milaneza',
+                img:'/assets/images/pizza/pizza3.png',
+                sizes:[
+                    {name:'Pequena', size:'120g',price:15.00},
+                    {name:'Médio', size:'320g',price:18.00},
+                    {name:'Grande', size:'820g',price:22.00}
+
+                ]
+    
+            }
+        ]
+    }
+
+
+]
--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const qs = (e)=>{
 const qsall = (e)=>{
     return document.querySelectorAll(e)
 }
+
+const cardSection = {
+    
+}
+
+products.forEach((section, index)=>{
+
+    let cardSection = qs('.card-section').cloneNode(true)
+
+    cardSection.querySelector('.img img').src = section.img
+    cardSection.querySelector('.card-section-title span').innerHTML = section.nameSection
+
+    cardSection.dataset.key = section.id
+
+    qs('.cards').appendChild(cardSection)
+})
