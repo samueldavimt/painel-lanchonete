0af19be0dda2fdba605b764bdce5c5754cd29988
Samuel Davi <samuel.davimont@gmail.com>
2022-02-28 20:52:39 -0300

    inserindo cards

--- js/products.js
+++ js/products.js
@@ -8,8 +8,9 @@ const products = [
         items: [
             {
                 id:1, 
-                name:'Burger Milaneza',
+                name:'Burger Caseiro',
                 img:'/assets/images/burger/burger1.jpg',
+                price:10.00,
                 sizes:[
                     {name:'Pequena', size:'120g',price:10.00},
                     {name:'Médio', size:'320g',price:15.00},
@@ -20,11 +21,12 @@ const products = [
             },
 
             {
-                id:1, 
-                name:'Burger Milaneza',
-                img:'/assets/images/burger/burger1.jpg',
+                id:2, 
+                name:'Burger Natural',
+                img:'/assets/images/burger/burger2.jpg',
+                price:12.00,
                 sizes:[
-                    {name:'Pequena', size:'120g',price:10.00},
+                    {name:'Pequena', size:'120g',price:12.00},
                     {name:'Médio', size:'320g',price:15.00},
                     {name:'Grande', size:'820g',price:22.00}
 
@@ -33,11 +35,12 @@ const products = [
             },
 
             {
-                id:1, 
+                id:3, 
                 name:'Burger Milaneza',
-                img:'/assets/images/burger/burger1.jpg',
+                img:'/assets/images/burger/burger3.jpg',
+                price:12.50,
                 sizes:[
-                    {name:'Pequena', size:'120g',price:10.00},
+                    {name:'Pequena', size:'120g',price:12.50},
                     {name:'Médio', size:'320g',price:15.00},
                     {name:'Grande', size:'820g',price:22.00}
 
@@ -57,6 +60,7 @@ const products = [
                 id:1, 
                 name:'Calabreza',
                 img:'/assets/images/pizza/pizza.png',
+                price:15.00,
                 sizes:[
                     {name:'Pequena', size:'120g',price:15.00},
                     {name:'Médio', size:'320g',price:17.00},
@@ -70,6 +74,7 @@ const products = [
                 id:2, 
                 name:'4 Queijos',
                 img:'/assets/images/pizza/pizza2.png',
+                price:20.00,
                 sizes:[
                     {name:'Pequena', size:'120g',price:20.00},
                     {name:'Médio', size:'320g',price:24.00},
@@ -83,8 +88,9 @@ const products = [
                 id:3, 
                 name:'Milaneza',
                 img:'/assets/images/pizza/pizza3.png',
+                price:15.50,
                 sizes:[
-                    {name:'Pequena', size:'120g',price:15.00},
+                    {name:'Pequena', size:'120g',price:15.50},
                     {name:'Médio', size:'320g',price:18.00},
                     {name:'Grande', size:'820g',price:22.00}
 
--- js/script.js
+++ js/script.js
@@ -6,18 +6,82 @@ const qsall = (e)=>{
     return document.querySelectorAll(e)
 }
 
-const cardSection = {
-    
+const insertCards = {
+    insertItems(){
+        qs('.cards').innerHTML = ''
+
+        let key = this.dataset.key 
+        console.log(key)
+        let sectionProducts = products.find(p=>{
+            return p.id == key
+        })
+        console.log(sectionProducts)
+        
+        let items = sectionProducts.items
+        items.forEach((item,index)=>{
+            let cardItem = qs('.card-item').cloneNode(true)
+
+            cardItem.querySelector('.card-item .img img').src = item.img
+
+            cardItem.querySelector('.card-item-name span').innerHTML = item.name
+
+            cardItem.querySelector('.card-item-price').innerHTML = item.price.toFixed(2,0)
+
+            cardItem.dataset.keyItem = index
+            cardItem.dataset.keySection = products.indexOf(sectionProducts)
+            
+
+            cardItem.addEventListener('click',windowItemInfo.displayWindowInfo)
+
+            qs('.cards').appendChild(cardItem)
+
+
+        })
+  
+    },
+
+    insertCardSection(){
+        qs('.cards').innerHTML = ''
+
+        products.forEach((section, index)=>{
+
+            let cardSection = qs('.card-section').cloneNode(true)
+        
+            cardSection.querySelector('.img img').src = section.img
+            cardSection.querySelector('.card-section-title span').innerHTML = section.nameSection
+        
+            cardSection.dataset.key = section.id
+        
+            cardSection.addEventListener('click',insertCards.insertItems)
+        
+            qs('.cards').appendChild(cardSection)
+        })
+
+    }
+
 }
 
-products.forEach((section, index)=>{
+const windowItemInfo = {
+    displayWindowInfo(){
+        let windowInfo = qs('.item-window-container')
 
-    let cardSection = qs('.card-section').cloneNode(true)
+        windowInfo.style.opacity  = 0
 
-    cardSection.querySelector('.img img').src = section.img
-    cardSection.querySelector('.card-section-title span').innerHTML = section.nameSection
+        setTimeout(()=>{
+            windowInfo.style.opacity = 1 
+        },200)
 
-    cardSection.dataset.key = section.id
+        windowInfo.style.display = 'flex'
 
-    qs('.cards').appendChild(cardSection)
-})
+        
+        
+        let positionSection = this.dataset.keySection
+        let positionItem = this.dataset.keyItem
+        let item = products[positionSection].items[positionItem]
+
+
+    }
+
+}
+
+insertCards.insertCardSection()
